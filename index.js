b98ab9ff13e55be7c5292d1524eec1856b7190e3
Akram-Ul-Hasan <akramulhasan66@gmail.com>
2023-04-17 23:16:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const phones = require("./phones.json");
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from my first ever server using nodemon");
+});
+
+app.get("/phones", (req, res) => {
+  res.send(phones);
+});
+
+app.get("/phones/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const phone = phones.find((phone) => phone.id === id) || {};
+  res.send(phone);
+
+  //console.log('I need data for id:',id);
+});
+
+app.get("/data", (req, res) => {
+  res.send("More data are coming soon");
+});
+
+app.listen(port, () => {
+  console.log(`My first ever server running on port: ${port}`);
+});
